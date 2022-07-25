d829b75ee51faeb2c7b9c7dc9a7f8bd8769c2540
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-07-25 13:57:59 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cocktail-app',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
