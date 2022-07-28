2468e2af5a283755607f861c30fb3668ea0015df
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-07-27 23:19:15 -0400

    modified tab.

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Random Cocktail</ion-title>
+        <ion-title>Random Cocktail App</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content v-if="state.loading">
